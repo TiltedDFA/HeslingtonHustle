e69850cb89b6e0882dc705ab95a5b83fd580919b
Malik T <maliktremain1@gmail.com>
2024-03-06 13:05:41 +0000

    fixed naming error

--- desktop/src/com/waddle_ware/heslington_hussle/DesktopLauncher.java
+++ desktop/src/com/waddle_ware/heslington_hussle/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("HestlingtonHussle");
+		config.setTitle("HestlingtonHustle");
 		new Lwjgl3Application(new Game(), config);
 	}
 }
